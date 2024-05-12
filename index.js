9d481237d29104761cb047988a5b4d9646e84df0
TI Sabbir <tisabbir0@gmail.com>
2024-05-12 07:36:16 +0600

    v types get added and the collection now can be accessed from browser

--- index.js
+++ index.js
@@ -32,6 +32,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // types collection db
+
+    const typesCollection = client.db('volunteeringDB').collection('typesCollection');
+
+    
+    app.get('/types', async(req, res)=> {
+        const types = await typesCollection.find().toArray();
+        res.send(types)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
