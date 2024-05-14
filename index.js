ed9d646cbbae3739d08aeb8ca151209362d27f75
TI Sabbir <tisabbir0@gmail.com>
2024-05-14 15:08:29 +0600

    delete addded

--- index.js
+++ index.js
@@ -80,6 +80,14 @@ async function run() {
         res.send(result);
     })
 
+    //delete posts
+    app.delete('/post/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id)}
+        const result = await postCollection.deleteOne(query)
+        res.send(result)
+    })
+
 
     //volunteer Request
 
@@ -95,6 +103,8 @@ async function run() {
         res.send(result);
     })
 
+
+    
 
 
 
